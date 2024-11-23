433ff9c13d36b5189849139563703b58b0b733cb
Maryanne Wachter <maryanne.wachter@gmail.com>
2024-11-23 07:41:55 -0800

    chore: guardrail on shell script

--- src/complaints.csv.sh
+++ src/complaints.csv.sh
@@ -2,7 +2,7 @@
 
 DATE=$(date +%Y-%m-%d)
 
-cd ~/workspaces/nyc-dob-observable/src/data
+cd ~/workspaces/nyc-dob-observable/src/data || exit
 ~/.virtualenvs/nyc-dob/bin/python3 complaints.py
 
 tippecanoe -z20 -o "nyc-buildings.pmtiles" -r1 '--cluster-distance=1' --cluster-densest-as-needed --no-tile-size-limit --extend-zooms-if-still-dropping "${DATE}_nycdob_rollup.json" -l "nyc-buildings" --force
